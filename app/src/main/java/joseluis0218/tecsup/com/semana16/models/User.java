6251225a49f69614ee32493877de0b5b7a7035c7
Jose <jose.cristobal@tecsup.edu.pe>
2018-12-03 23:35:22 -0500

    Initial commit

---
+++ app/src/main/java/joseluis0218/tecsup/com/semana16/models/User.java
@@ -0,0 +1,66 @@
+package joseluis0218.tecsup.com.semana16.models;
+
+public class User {
+
+    private String id;
+
+    private String email;
+
+    private String fullname;
+
+    private String latitud;
+
+    private String longitud;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+
+    public String getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        this.longitud = longitud;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", email='" + email + '\'' +
+                ", fullname='" + fullname + '\'' +
+                ", latitud=" + latitud +
+                ", longitud=" + longitud +
+                '}';
+    }
+}
